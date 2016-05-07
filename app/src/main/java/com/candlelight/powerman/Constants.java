3955db9cf228e50fb917b79e5aa3e5a59e94c18a
0rangefist <collinskthomas@gmail.com>
2016-05-07 15:27:28 +0000

    - Fragments structure setup - Control Fragment done  - Schedule Fragment done  - Yet to do Monitor & PowerPlan

---
+++ app/src/main/java/com/candlelight/powerman/Constants.java
@@ -0,0 +1,11 @@
+package com.candlelight.powerman;
+
+/**
+ * Created by Thomas on 07/05/2016.
+ */
+public class Constants {
+    public static final String Monitor = "Monitor";
+    public static final String Control = "Control";
+    public static final String Schedule = "Schedule";
+    public static final String PowerPlan = "Power Plan";
+}
